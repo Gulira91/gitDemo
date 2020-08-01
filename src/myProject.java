7e8b3683cc60aa14c71911dd9998c6de8c636b29
Gulira91 <68974279+Gulira91@users.noreply.github.com>
2020-08-01 16:37:26 -0400

    Update myProject.java

--- src/myProject.java
+++ src/myProject.java
@@ -2,5 +2,7 @@ public class myProject {
     public static void main(String[] args) {
          System.out.println("Hello ");
         System.out.println("Hello from Github");
+        // hello
+        
     }
 }
