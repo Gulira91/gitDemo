2bc411bdf73dff5015e64e6a6e1710ed9affc1a8
Gulira91 <d.gulira@gmail.com>
2020-08-01 12:43:37 -0400

    update myFirstGitClass.java

--- src/myFirstGitClass.java
+++ src/myFirstGitClass.java
@@ -1,5 +1,6 @@
 public class myFirstGitClass {
     public static void main(String[] args) {
         System.out.println("Its going prety well so far");
+        System.out.println("i love java");
     }
 }
