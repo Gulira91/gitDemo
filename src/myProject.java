08ecc03542780bc1e40541d8fd20bf50fa6b35e1
Gulira91 <d.gulira@gmail.com>
2020-08-01 21:14:19 -0400

    myProject.java

--- src/myProject.java
+++ src/myProject.java
@@ -1,5 +1,7 @@
 public class myProject {
     public static void main(String[] args) {
          System.out.println("Hello ");
+        System.out.println("");
+        System.out.println("Friday");
     }
 }
